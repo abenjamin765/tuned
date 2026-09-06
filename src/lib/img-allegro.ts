3e0863b69562d1c3da2269fa2b75eb760d057ed2
Aaron Benjamin <abenjamin765@gmail.com>
2026-09-06 19:33:29 -0400

    Refactor image embeds into combiner modules importing split data-URL parts

--- src/lib/img-allegro.ts
+++ src/lib/img-allegro.ts
@@ -1,1 +1,3 @@
-file:///workspace/tuned/src/lib/img-allegro.ts
+import { allegroImagePartA } from '@/lib/img-allegro-a';
+import { allegroImagePartB } from '@/lib/img-allegro-b';
+export const allegroImage = 'data:image/jpeg;base64,' + allegroImagePartA + allegroImagePartB;
