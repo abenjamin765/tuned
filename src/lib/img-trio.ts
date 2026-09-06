e2f3ef81c254013ea53f5ca349c972801f2da800
Aaron Benjamin <abenjamin765@gmail.com>
2026-09-06 19:49:39 -0400

    Expand trio image embed combiner to eight smaller base64 parts

--- src/lib/img-trio.ts
+++ src/lib/img-trio.ts
@@ -4,4 +4,6 @@ import { trioImagePartC } from '@/lib/img-trio-c';
 import { trioImagePartD } from '@/lib/img-trio-d';
 import { trioImagePartE } from '@/lib/img-trio-e';
 import { trioImagePartF } from '@/lib/img-trio-f';
-export const trioImage = 'data:image/jpeg;base64,' + trioImagePartA + trioImagePartB + trioImagePartC + trioImagePartD + trioImagePartE + trioImagePartF;
+import { trioImagePartG } from '@/lib/img-trio-g';
+import { trioImagePartH } from '@/lib/img-trio-h';
+export const trioImage = 'data:image/jpeg;base64,' + trioImagePartA + trioImagePartB + trioImagePartC + trioImagePartD + trioImagePartE + trioImagePartF + trioImagePartG + trioImagePartH;
