bec5045348dbd884228bfc6a4e6feb21a9acae7d
Aaron Benjamin <abenjamin765@gmail.com>
2026-09-06 19:40:28 -0400

    Expand andante image embed combiner to four base64 parts

--- src/lib/img-andante.ts
+++ src/lib/img-andante.ts
@@ -1,3 +1,5 @@
 import { andanteImagePartA } from '@/lib/img-andante-a';
 import { andanteImagePartB } from '@/lib/img-andante-b';
-export const andanteImage = 'data:image/jpeg;base64,' + andanteImagePartA + andanteImagePartB;
+import { andanteImagePartC } from '@/lib/img-andante-c';
+import { andanteImagePartD } from '@/lib/img-andante-d';
+export const andanteImage = 'data:image/jpeg;base64,' + andanteImagePartA + andanteImagePartB + andanteImagePartC + andanteImagePartD;
